6abb4f22516766f794bbe9953d80084ecf0e50e8
Zach <zlammie@gmail.com>
2025-10-23 09:21:07 -0500

    hotfix 10.23

--- server/middleware/requireSameCompany.js
+++ server/middleware/requireSameCompany.js
@@ -1,9 +1,5 @@
 module.exports = function requireSameCompany(req, res, next) {
-  // For admin pages that manage users in the same company
-  module.exports = function requireSameCompany(req, res, next) {
-  const companyId = req.user?.company || req.session?.user?.companyId; // fallback for legacy
+  const companyId = req.user?.company || req.session?.user?.companyId;
   req.scope = { companyId };
-  next();
-};
   next();
 };
